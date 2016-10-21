b3cdc92f58ad0f5c5ea800975de06f5ca12c8c81
Alaa SBAIHI <sbaihi.alaa@gmail.com>
2016-10-21 20:57:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Redi m on 10/21/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello redi");
+
+    }
+}
